e2be932cfb86fb1f5b7b23b36237ea43fc856e4d
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-06-25 17:48:01 +0200

    Add tests for variadic args

--- packages/xmock/tests/node/specs/errors.spec.ts
+++ packages/xmock/tests/node/specs/errors.spec.ts
@@ -25,7 +25,7 @@ describe('XMock', () => {
     });
 
 
-    it('method called with wrong args', () => {
+    it('method called with wrong arg', () => {
       interface Foo {
         bar(x: number): void;
       }
@@ -34,6 +34,54 @@ describe('XMock', () => {
       mock.when(f => f.bar(23)).returns(undefined);
 
       expect(() => mock.object.bar(21)).to.throw(/21(.*)23/s);
+    });
+
+    it('method called with wrong args', () => {
+      interface Foo {
+        bar(x: number, y: number): void;
+      }
+
+      const mock = new XMock<Foo>();
+      mock.when(f => f.bar(1, 2)).returns(undefined);
+
+      expect(() => mock.object.bar(3, 4))
+        .to.throw(/3(.*)4(.*)1(.*)2/s);
+    });
+
+    it('method called with less variadic args', () => {
+      interface Foo {
+        bar(...args: number[]): void;
+      }
+
+      const mock = new XMock<Foo>();
+      mock.when(f => f.bar(1, 2, 3)).returns(undefined);
+
+      expect(() => mock.object.bar(1, 2))
+        .to.throw(/1(.*)2(.*)1(.*)2(.*)3/s);
+    });
+
+    it('method called with more variadic args', () => {
+      interface Foo {
+        bar(...args: number[]): void;
+      }
+
+      const mock = new XMock<Foo>();
+      mock.when(f => f.bar(1, 2)).returns(undefined);
+
+      expect(() => mock.object.bar(1, 2, 3))
+        .to.throw(/1(.*)2(.*)3(.*)1(.*)2/s);
+    });
+
+    it('method called with wrong variadic args', () => {
+      interface Foo {
+        bar(...args: number[]): void;
+      }
+
+      const mock = new XMock<Foo>();
+      mock.when(f => f.bar(1, 2)).returns(undefined);
+
+      expect(() => mock.object.bar(3, 4))
+        .to.throw(/3(.*)4(.*)1(.*)2/s);
     });
   });
 });
--- packages/xmock/tests/node/specs/methods.spec.ts
+++ packages/xmock/tests/node/specs/methods.spec.ts
@@ -25,17 +25,6 @@ describe('XMock', () => {
       expect(mock.object.bar()).to.equal('bar');
     });
 
-    it('1 arg and no return', () => {
-      interface Foo {
-        bar(x: string): void;
-      }
-
-      const mock = new XMock<Foo>();
-      mock.when(f => f.bar('bar')).returns(undefined);
-
-      expect(mock.object.bar('bar')).to.be.undefined;
-    });
-
     it('1 arg and return', () => {
       interface Foo {
         bar(x: string): number;
@@ -47,17 +36,6 @@ describe('XMock', () => {
       expect(mock.object.bar('bar')).to.equal(23);
     });
 
-    it('2 args and no return', () => {
-      interface Foo {
-        bar(x: string, y: number): void;
-      }
-
-      const mock = new XMock<Foo>();
-      mock.when(f => f.bar('bar', 21)).returns(undefined);
-
-      expect(mock.object.bar('bar', 21)).to.be.undefined;
-    });
-
     it('2 args and return', () => {
       interface Foo {
         bar(x: string, y: boolean): number;
@@ -67,6 +45,17 @@ describe('XMock', () => {
       mock.when(f => f.bar('bar', true)).returns(23);
 
       expect(mock.object.bar('bar', true)).to.equal(23);
+    });
+
+    it('variadic args and return', () => {
+      interface Foo {
+        bar(...args: number[]): number;
+      }
+
+      const mock = new XMock<Foo>();
+      mock.when(f => f.bar(1, 2, 3, 4)).returns(23);
+
+      expect(mock.object.bar(1, 2, 3, 4)).to.equal(23);
     });
   });
 });
