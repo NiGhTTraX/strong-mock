d0d00fc047a5fd32ce32f246777ab12bf4cd55be
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-06-25 17:33:03 +0200

    Add some more basic tests

--- packages/xmock/tests/node/specs/methods.spec.ts
+++ packages/xmock/tests/node/specs/methods.spec.ts
@@ -46,5 +46,27 @@ describe('XMock', () => {
 
       expect(mock.object.bar('bar')).to.equal(23);
     });
+
+    it('2 args and no return', () => {
+      interface Foo {
+        bar(x: string, y: number): void;
+      }
+
+      const mock = new XMock<Foo>();
+      mock.when(f => f.bar('bar', 21)).returns(undefined);
+
+      expect(mock.object.bar('bar', 21)).to.be.undefined;
+    });
+
+    it('2 args and return', () => {
+      interface Foo {
+        bar(x: string, y: boolean): number;
+      }
+
+      const mock = new XMock<Foo>();
+      mock.when(f => f.bar('bar', true)).returns(23);
+
+      expect(mock.object.bar('bar', true)).to.equal(23);
+    });
   });
 });
