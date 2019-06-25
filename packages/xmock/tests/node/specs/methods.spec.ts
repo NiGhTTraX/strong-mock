c88f93d4a5ad13f5297cd6d9a2701a943c428581
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-06-25 17:31:55 +0200

    Add some basic method tests

---
+++ packages/xmock/tests/node/specs/methods.spec.ts
@@ -0,0 +1,50 @@
+import { describe, expect, it } from '../../../../../tests/node/suite';
+import XMock from 'xmock';
+
+describe('XMock', () => {
+  describe('method expectations', () => {
+    it('no args and no return', () => {
+      interface Foo {
+        bar(): void;
+      }
+
+      const mock = new XMock<Foo>();
+      mock.when(f => f.bar()).returns(undefined);
+
+      expect(mock.object.bar()).to.be.undefined;
+    });
+
+    it('no args and return', () => {
+      interface Foo {
+        bar(): string;
+      }
+
+      const mock = new XMock<Foo>();
+      mock.when(f => f.bar()).returns('bar');
+
+      expect(mock.object.bar()).to.equal('bar');
+    });
+
+    it('1 arg and no return', () => {
+      interface Foo {
+        bar(x: string): void;
+      }
+
+      const mock = new XMock<Foo>();
+      mock.when(f => f.bar('bar')).returns(undefined);
+
+      expect(mock.object.bar('bar')).to.be.undefined;
+    });
+
+    it('1 arg and return', () => {
+      interface Foo {
+        bar(x: string): number;
+      }
+
+      const mock = new XMock<Foo>();
+      mock.when(f => f.bar('bar')).returns(23);
+
+      expect(mock.object.bar('bar')).to.equal(23);
+    });
+  });
+});
--- packages/xmock/tests/node/specs/xmock.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from '../../../../../tests/node/suite';
-import XMock from '../../../src';
-
-describe('XMock', () => {
-  it('should not blow up', () => {
-    // eslint-disable-next-line no-new
-    new XMock();
-  });
-});
