555ae1bad944f3a0ab5887e6b3e279b139caf7a4
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-06-26 09:43:53 +0200

    Refactor differentiating between method and property expectations

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,17 @@
 import { inspect, isDeepStrictEqual } from 'util';
 
-interface Expectation<T> {
-  args?: any[];
-  r: any;
-  met: boolean;
-}
-
 export type Stub<T, R> = {
   returns(r: R): void;
 }
 
-class CallExpectation<T> implements Expectation<T> {
-  public args: any;
+class MethodExpectation {
+  public args: any[];
 
   public r: any;
 
   public met: boolean;
 
-  constructor(args: any, r: any) {
+  constructor(args: any[], r: any) {
     this.args = args;
     this.met = false;
     this.r = r;
@@ -28,7 +22,7 @@ class CallExpectation<T> implements Expectation<T> {
   }
 }
 
-class PropertyExpectation<T> implements Expectation<T> {
+class PropertyExpectation {
   public r: any;
 
   public met: boolean;
@@ -44,13 +38,15 @@ class PropertyExpectation<T> implements Expectation<T> {
 }
 
 /**
- * Mock interfaces and set expectations.
+ * Mock interfaces and set method and property expectations.
  *
  * Mocks are strict by default - an unexpected call will throw an error
  * and so will a call with more params than expected.
  */
 export default class Mock<T> {
-  private expectations: Map<string, Expectation<T>[]> = new Map();
+  private methodExpectations: Map<string, MethodExpectation[]> = new Map();
+
+  private propertyExpectations: Map<string, PropertyExpectation> = new Map();
 
   // TODO: implement It.isAny
   when<R>(cb: (fake: T) => R): Stub<T, R> {
@@ -71,12 +67,21 @@ export default class Mock<T> {
 
     return {
       returns: (r: R) => {
-        this.expectations.set(expectedProperty, [
-          ...(this.expectations.get(expectedProperty) || []),
-          expectedArgs
-            ? new CallExpectation(expectedArgs, r)
-            : new PropertyExpectation(r)
-        ]);
+        if (expectedArgs) {
+          this.methodExpectations.set(
+            expectedProperty,
+            [
+              ...(this.methodExpectations.get(expectedProperty) || []),
+              new MethodExpectation(expectedArgs, r)
+            ]
+          );
+        } else {
+          // TODO: support multiple expectations
+          this.propertyExpectations.set(
+            expectedProperty,
+            new PropertyExpectation(r)
+          );
+        }
       }
     };
   }
@@ -84,28 +89,31 @@ export default class Mock<T> {
   get stub(): T {
     return new Proxy({}, {
       get: (target, property: string) => {
-        const expectationsForProperty = this.expectations.get(property);
+        const propertyExpectation = this.propertyExpectations.get(property);
 
-        if (!expectationsForProperty) {
-          throw new Error(`${property} not expected to be called`);
+        if (propertyExpectation) {
+          propertyExpectation.met = true;
+
+          return propertyExpectation.r;
         }
 
-        // TODO: this only checks the first expectation
-        // TODO: cleaner check for property vs call expectations
-        if (!expectationsForProperty[0].args) {
-          expectationsForProperty[0].met = true;
+        const methodExpectations = this.methodExpectations.get(property);
 
-          return expectationsForProperty[0].r;
+        if (!methodExpectations) {
+          // Since we don't have any property or method expectations we can't tell
+          // if the requested property is a method or just a getter, therefore we
+          // throw a generic message.
+          throw new Error(`${property} not expected to be called`);
         }
 
         return (...args: any[]) => {
           // Find the first unmet expectation.
-          const expectation = expectationsForProperty.find(
+          const expectation = methodExpectations.find(
             // We check in both directions to
             // 1) catch extra args that were not expected and
             // 2) treat `undefined` and missing optional args as equal.
-            e => e.args!.every((arg, i) => isDeepStrictEqual(args[i], arg))
-              && args.every((arg, i) => isDeepStrictEqual(e.args![i], arg))
+            e => e.args.every((arg, i) => isDeepStrictEqual(args[i], arg))
+              && args.every((arg, i) => isDeepStrictEqual(e.args[i], arg))
           );
 
           if (expectation) {
@@ -117,27 +125,30 @@ export default class Mock<T> {
           throw new Error(`${property} not expected to be called with ${inspect(args)}!
 
 Existing expectations:
-${expectationsForProperty.join(' or ')}`);
+${methodExpectations.join(' or ')}`);
         };
       }
     }) as T;
   }
 
   verifyAll() {
-    this.expectations.forEach((expectationsForProperty, p) => {
-      expectationsForProperty.forEach(expectation => {
+    this.propertyExpectations.forEach((expectation, p) => {
+      if (!expectation.met) {
+        throw new Error(`Expected ${p} to be accessed ${expectation}`);
+      }
+    });
+
+    this.methodExpectations.forEach((expectations, p) => {
+      expectations.forEach(expectation => {
         if (!expectation.met) {
-          if (expectation.args) {
-            throw new Error(`Expected ${p} to be called with ${expectation}`);
-          } else {
-            throw new Error(`Expected ${p} to be accessed ${expectation}`);
-          }
+          throw new Error(`Expected ${p} to be called with ${expectation}`);
         }
       });
     });
   }
 
   reset() {
-    this.expectations.clear();
+    this.propertyExpectations.clear();
+    this.methodExpectations.clear();
   }
 }
