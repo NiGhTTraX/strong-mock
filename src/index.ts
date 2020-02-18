70ee0fc5daf7e2a2f2f84439bd0014bc3ba8abd4
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-02-18 20:52:58 +0100

    Throw if previous expectation is missing a return value

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
-type Mock<T> = T;
+export type Mock<T> = T;
 
-export const strongMock = <T>(): Mock<T> => ((() => {}) as unknown) as Mock<T>;
+let pendingExpectation = 0;
+
+export class MissingReturnValue extends Error {
+  constructor() {
+    super(`You forgot to give a return value to the previous expectation`);
+  }
+}
+
+export const strongMock = <T>(): Mock<T> => {
+  pendingExpectation = 0;
+
+  return ((() => {}) as unknown) as Mock<T>;
+};
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
-export const when = <T>(x: T) => {};
+export const when = <T>(x: T) => {
+  if (pendingExpectation) {
+    throw new MissingReturnValue();
+  }
+  pendingExpectation = 1;
+};
--- tests/when.spec.ts
+++ tests/when.spec.ts
@@ -1,10 +1,19 @@
+import { expect } from 'tdd-buffet/expect/jest';
 import { describe, it } from 'tdd-buffet/suite/node';
-import { strongMock, when } from '../src';
+import { MissingReturnValue, strongMock, when } from '../src';
 
 describe('when', () => {
   it('should do nothing without a chained return', () => {
     const mock = strongMock<() => void>();
 
     when(mock());
+  });
+
+  it('should throw if previous expectation is missing return', () => {
+    const mock = strongMock<() => void>();
+
+    when(mock());
+
+    expect(() => when(mock())).toThrow(MissingReturnValue);
   });
 });
