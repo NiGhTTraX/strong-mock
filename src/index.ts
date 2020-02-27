c89386eaa8b4dedaa8c52a1e9ef14b1319c3346a
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-02-27 22:05:58 +0100

    Export verifyAll from index

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@
 import { instance } from './instance';
 import { strongMock } from './mock';
 import { when } from './when';
+import { verifyAll } from './verify';
 
-export { strongMock, when, instance };
+export { strongMock, when, instance, verifyAll };
