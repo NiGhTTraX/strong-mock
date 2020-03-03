10207f7e942cd4c981d64a490328683cf10cccc9
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-03-03 12:44:02 +0100

    Export It from index

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import { mock } from './mock';
 import { reset } from './reset';
 import { verify } from './verify';
 import { when } from './when';
+import { It } from './matcher';
 
-export { mock, when, instance, verify, reset };
+export { mock, when, instance, verify, reset, It };
