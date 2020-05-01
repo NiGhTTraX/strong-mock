483d5fa6ee33943c80e09968c7a1a04b5051f5eb
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-05-01 17:13:00 +0200

    docs: Update docstring for `when`

--- src/when.ts
+++ src/when.ts
@@ -4,24 +4,28 @@ import { createReturns, Stub } from './returns';
 /**
  * Set an expectation on a mock.
  *
- * The expectation must be finished by setting a return value. By default, the
- * call is expected to only be made once. Use the invocation count helpers to
- * expect a call multiple times.
+ * The expectation must be finished by setting a return value, even if the value
+ * is `undefined`.
  *
  * If a call happens that was not expected then the mock will throw an error.
+ * By default, the call is expected to only be made once. Use the invocation
+ * count helpers to expect a call multiple times.
+ *
+ * @param expectedCall Make a "real" call using the value returned by `mock()`.
  *
  * @example
- * when(mock(1, 2, 3)).thenReturn(42);
+ * const fn = mock<() => void>();
+ * when(fn()).thenReturn(undefined);
  *
  * @example
- * const foo = mock<() => number>();
+ * const fn = mock<() => number>();
+ * when(fn()).thenReturn(42).atMost(3);
  *
- * when(foo()).thenReturn('baz'); // type error, expected number
- *
- * @param expectation The type of this will be used in the calls that set
- *   a return value.
+ * @example
+ * const fn = mock<(x: number) => Promise<number>();
+ * when(fn(23)).thenResolve(42);
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
-export const when = <R>(expectation: R): Stub<R> => {
+export const when = <R>(expectedCall: R): Stub<R> => {
   return createReturns<R>(getMockState(getActiveMock()).pendingExpectation);
 };
