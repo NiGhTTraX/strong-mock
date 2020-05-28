cc5b0a8d125c3632f6c6610381b4fc78b8994f7f
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-05-28 14:28:39 +0200

    feat: Add isNumber matcher
    ```typescript
    const fn = mock<(x: number) => number>();
    when(fn(It.isNumber()).returns(42);
    
    instance(fn)(20.5) === 42
    instance(fn)(NaN) // throws
    ```

--- src/matcher.ts
+++ src/matcher.ts
@@ -35,6 +35,7 @@ export function isMatcher(f: any): f is Matcher<any> {
  * @example
  * const fn = mock<(x: number, y: string) => number>();
  * when(fn(It.isAny(), It.isAny()).thenReturn(1);
+ *
  * instance(fn)(23, 'foobar') === 1
  */
 const isAny = (): Matcher<any> => ({
@@ -57,6 +58,8 @@ const isAny = (): Matcher<any> => ({
  * @example
  * const fn = mock<(x: number) => number>();
  * when(fn(It.matches(x => x >= 0)).returns(42);
+ *
+ * instance(fn)(2) === 42
  * instance(fn)(-1) // throws
  */
 const matches = <T>(cb: (arg: T) => boolean): Matcher<T> =>
@@ -77,6 +80,7 @@ const matches = <T>(cb: (arg: T) => boolean): Matcher<T> =>
  * @example
  * const fn = mock<(foo: { x: number, y: number }) => number>();
  * when(fn(It.isObjectContaining({ x: 23 }).returns(42);
+ *
  * instance(fn)({ x: 100, y: 200 }) // throws
  * instance(fn)({ x: 23, y: 200 }) // returns 42
  */
@@ -92,6 +96,23 @@ const isObjectContaining = <T extends object, K extends DeepPartial<T>>(
   } as any);
 
 /**
+ * Match any number.
+ *
+ * @example
+ * const fn = mock<(x: number) => number>();
+ * when(fn(It.isNumber()).returns(42);
+ *
+ * instance(fn)(20.5) === 42
+ * instance(fn)(NaN) // throws
+ */
+const isNumber = (): Matcher<number> =>
+  ({
+    __isMatcher: true,
+    matches: (arg: any) => typeof arg === 'number' && !Number.isNaN(arg),
+    toJSON: () => 'isNumber',
+  } as any);
+
+/**
  * Contains argument matchers that can be used to ignore arguments in an
  * expectation or to match complex arguments.
  */
@@ -100,4 +121,5 @@ export const It = {
   isAny,
   matches,
   isObjectContaining,
+  isNumber,
 };
