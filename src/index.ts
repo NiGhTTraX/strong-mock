dde189fc2ca3f2dba4f8da5b125eb071968b9ba6
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2021-09-26 13:52:44 +0200

    refactor: Use `export from` in index

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,11 @@
 /* istanbul ignore file */
-import { It } from './expectation/it';
-import { instance } from './instance/instance';
-import { Matcher } from './expectation/matcher';
-import { setDefaults, StrongMockDefaults } from './mock/defaults';
-import { mock } from './mock/mock';
-import { reset, resetAll } from './verify/reset';
-import { verify, verifyAll } from './verify/verify';
-import { when } from './when/when';
+export { mock } from './mock/mock';
+export { when } from './when/when';
+export { instance } from './instance/instance';
+export { reset, resetAll } from './verify/reset';
+export { verify, verifyAll } from './verify/verify';
+export { It } from './expectation/it';
+export { setDefaults } from './mock/defaults';
 
-export {
-  mock,
-  when,
-  instance,
-  verify,
-  verifyAll,
-  reset,
-  resetAll,
-  It,
-  setDefaults,
-};
-
-export type { StrongMockDefaults, Matcher };
+export type { Matcher } from './expectation/matcher';
+export type { StrongMockDefaults } from './mock/defaults';
