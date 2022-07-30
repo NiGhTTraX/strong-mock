71bb975e1af9a5b748303a07a90dd43e03fc96fd
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2022-07-30 17:25:53 +0200

    feat: Add strictness option to improve unexpected call errors

--- src/expectation/repository/strong-repository.spec.ts
+++ src/expectation/repository/strong-repository.spec.ts
@@ -1,12 +1,12 @@
 import { UnexpectedAccess, UnexpectedCall } from '../../errors';
 import { ApplyProp } from '../expectation';
-import { StrongRepository } from './strong-repository';
 import {
   MatchingCallExpectation,
   MatchingPropertyExpectation,
   NotMatchingExpectation,
 } from '../expectation.mocks';
 import { repoContractTests } from './repo.contract';
+import { Strictness, StrongRepository } from './strong-repository';
 
 describe('StrongRepository', () => {
   Object.entries(repoContractTests).forEach(([suite, tests]) => {
@@ -17,38 +17,59 @@ describe('StrongRepository', () => {
     });
   });
 
-  it('should throw if no property expectations match', () => {
-    const repo = new StrongRepository();
+  describe('super strict', () => {
+    it('should throw if no property expectations match', () => {
+      const repo = new StrongRepository();
 
-    expect(() => repo.get('whatever')).toThrow(UnexpectedAccess);
+      expect(() => repo.get('whatever')).toThrow(UnexpectedAccess);
+    });
+
+    it('should throw if no call expectations match', () => {
+      const repo = new StrongRepository();
+      repo.add(new NotMatchingExpectation('foo', { value: 23 }));
+
+      expect(() => repo.get('foo').value(3, 4)).toThrow(UnexpectedCall);
+    });
+
+    it('should throw if no apply expectations', () => {
+      const repo = new StrongRepository();
+
+      expect(() => repo.get(ApplyProp).value(1, 2, 3)).toThrow(UnexpectedCall);
+    });
+
+    it('should throw after a property expectation is fulfilled', () => {
+      const repo = new StrongRepository();
+      repo.add(new MatchingPropertyExpectation('foo', { value: 23 }));
+      repo.get('foo');
+
+      expect(() => repo.get('foo')).toThrow(UnexpectedAccess);
+    });
+
+    it('should throw after a function expectation is fulfilled', () => {
+      const repo = new StrongRepository();
+      repo.add(new MatchingCallExpectation('foo', { value: 23 }));
+      repo.get('foo').value(1, 2);
+
+      expect(() => repo.get('foo')).toThrow(UnexpectedAccess);
+    });
   });
 
-  it('should throw if no call expectations match', () => {
-    const repo = new StrongRepository();
-    repo.add(new NotMatchingExpectation('foo', { value: 23 }));
+  describe('medium strict', () => {
+    it('should return the matching property expectation', () => {
+      const repo = new StrongRepository(Strictness.STRICT);
 
-    expect(() => repo.get('foo').value(3, 4)).toThrow(UnexpectedCall);
-  });
+      repo.add(new MatchingPropertyExpectation('foo', { value: 23 }));
 
-  it('should throw if no apply expectations', () => {
-    const repo = new StrongRepository();
+      expect(repo.get('foo').value).toEqual(23);
+    });
 
-    expect(() => repo.get(ApplyProp).value(1, 2, 3)).toThrow(UnexpectedCall);
-  });
+    it('should return a function that throws unexpected call for properties with no expectations', () => {
+      const repo = new StrongRepository(Strictness.STRICT);
 
-  it('should throw after a property expectation is fulfilled', () => {
-    const repo = new StrongRepository();
-    repo.add(new MatchingPropertyExpectation('foo', { value: 23 }));
-    repo.get('foo');
+      const { value } = repo.get('foo');
 
-    expect(() => repo.get('foo')).toThrow(UnexpectedAccess);
-  });
-
-  it('should throw after a function expectation is fulfilled', () => {
-    const repo = new StrongRepository();
-    repo.add(new MatchingCallExpectation('foo', { value: 23 }));
-    repo.get('foo').value(1, 2);
-
-    expect(() => repo.get('foo')).toThrow(UnexpectedAccess);
+      expect(value).toBeInstanceOf(Function);
+      expect(() => value(1, 2, 3)).toThrow(UnexpectedCall);
+    });
   });
 });
