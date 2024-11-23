6c3a41c04e78324682f1faf75ae8b109a3659a15
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2024-11-23 18:17:50 +0100

    build: Inline and simplify jest config dependency

---
+++ tests/jest.config.ts
@@ -0,0 +1,27 @@
+import { createDefaultPreset, type JestConfigWithTsJest } from 'ts-jest';
+
+const presetConfig = createDefaultPreset({
+  tsconfig: {
+    target: 'ESNext',
+  },
+});
+
+const jestConfig: JestConfigWithTsJest = {
+  ...presetConfig,
+  testEnvironment: 'node',
+  rootDir: process.cwd(),
+  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
+  coverageDirectory: '<rootDir>/tests/results',
+  coverageReporters: ['lcov', 'text'],
+  coverageThreshold: {
+    global: {
+      lines: 100,
+      branches: 100,
+      functions: 100,
+      statements: 100,
+    },
+  },
+  collectCoverageFrom: ['**/src/**/*.{ts,tsx}', '!**/*.d.ts', '!**/*.mocks.ts'],
+};
+
+export default jestConfig;
