eb1b9d05fbf56f1b544db782dbca1780fdb30e44
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2025-07-17 13:20:22 +0200

    test: Alias the older strong-mock
    vitest doesn't seem to like importing it with the same name as the
    project.

--- tests/old.ts
+++ tests/old.ts
@@ -2,7 +2,7 @@
  * This is an older, battle tested version of strong-mock that we use to
  * test the current version.
  */
-import * as SM from 'strong-mock';
+import * as SM from 'sm-old';
 
 // Exporting manually instead of export * from because IntelliJ
 // can't autocomplete the import otherwise :(.
